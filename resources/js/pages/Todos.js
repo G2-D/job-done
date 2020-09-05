2ba92ea4c665d9651c34ad4d8f88903ec23fce85
Bruno Sampaio <brunobrsampaio@gmail.com>
2020-09-05 11:01:24 -0300

    Listagem de todos completados e nao completados

--- resources/js/pages/Todos.js
+++ resources/js/pages/Todos.js
@@ -7,11 +7,39 @@ const Todos = () => {
 
 	const { userId } = useParams();
 
-	const [todosList, setTodosList] = useState([]);
+	const [todosList, setTodosList] = useState({
+		completed 		: [],
+		notCompleted 	: []
+	});
+
+	const configTodosList = async () => {
+
+		const response = await getUserTodos(userId);
+
+		const completed 	= [];
+		const notCompleted 	= [];
+
+		response.forEach((todo) => {
+
+			if (todo.completed) {
+
+				completed.push(todo);
+			} else {
+
+				notCompleted.push(todo);
+			}
+		});
+
+
+		setTodosList({
+			completed,
+			notCompleted
+		});
+	};
 
 	const onInit = async () => {
 
-		setTodosList(await getUserTodos(userId));
+		configTodosList();
 	};
 
 	useEffect(() => {
@@ -21,8 +49,40 @@ const Todos = () => {
 
 	return (
 		<>	
-			{ console.log(todosList) }
-			Todos { userId }
+			<div>
+				<h2>
+					Não completados
+				</h2>
+				{
+					todosList.notCompleted.map((todo, index) => {
+
+						return (
+							<div key={index}>
+								<h3>
+									{ todo.title }
+								</h3>
+							</div>
+						);
+					})
+				}
+			</div>
+			<div>
+				<h2>
+					Completados
+				</h2>
+				{
+					todosList.completed.map((todo, index) => {
+
+						return (
+							<div key={index}>
+								<h3>
+									{ todo.title }
+								</h3>
+							</div>
+						);
+					})
+				}
+			</div>
 		</>
 	);
 };
